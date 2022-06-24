d003d3b5515b335a2b203471a08eea8e048ed451
James Seung Hyun Son <jamessoun93@gmail.com>
2022-06-24 19:10:46 +0900

    쓰기 지연, 변경 감지, Flush

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -16,24 +16,35 @@ public class JpaMain {
         tx.begin();
 
         try {
-            // 1차 캐시에 저장되는 것 확인용
+//            // 최초에는 1차 캐시에 아무것도 없기 때문에 select 문이 날라감
+//            // 이후에는 안날라가는 것 확인
+//
+//            Member member1 = em.find(Member.class, 101L);
+//            Member member2 = em.find(Member.class, 101L);
+//
+//            // 동일성 보장 (true)
+//            System.out.println(member1 == member2);
 
-            // 비영속
-            Member member = new Member();
-            member.setId(101L);
-            member.setName("HelloJPA");
 
-            // 영속
-            System.out.println("=== BEFORE ===");
-            em.persist(member); // 1차 캐시에 저장
-            System.out.println("=== AFTER ===");
+//            // 쓰기 지연
+//            Member member1 = new Member(150L, "A");
+//            Member member2 = new Member(160L, "B");
+//
+//            em.persist(member1);
+//            em.persist(member2);
+//
+//            System.out.println("=====================");
+//
+//            // 위 프린트문이 찍힌 다음 DB 쿼리가 날라가는것을 확인할 수 있음.
+//
+//            tx.commit();
 
-            // 1차 캐시로부터 가져옴 (select 문이 안나감)
-            Member foundMember = em.find(Member.class, 101L);
+            Member member = em.find(Member.class, 150L);
+            member.setName("ZZZZ");
 
-            System.out.println("foundMember.id = " + foundMember.getId());
-            System.out.println("foundMember.name = " + foundMember.getName());
+            System.out.println("========");
 
+            // persist 없이도 setter 통해서만 커밋 시점에 update 쿼리 날라가는 것 확인
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
